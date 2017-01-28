9a5ccd77df8ffdf358d6433712d5725b4fb48cee
DESKTOP-IQOQ25D\User <shishirove67@gmail.com>
2017-01-28 14:39:27 +0600

    First Commit
    Mars Balloon Blaster Project

---
+++ Assets/Balloon Bluster/Scripts/OnClickWebButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OnClickWebButton : MonoBehaviour 
+{
+	public bool isAppleStore;
+	public bool isGoogleStore;
+	public bool isAmazonStore;
+
+	public string urlAppleStore ;
+	public string urlGoogleStore= "https://play.google.com/store/apps/dev?id=8449310898275657078";
+	public string urlAmazonStore;
+
+	void Start()
+	{
+		GetComponent<Button> ().onClick.AddListener (() => {
+			if(isAppleStore)
+				Application.OpenURL(urlAppleStore);
+			else if(isGoogleStore)
+				Application.OpenURL(urlGoogleStore);
+			else if(isAmazonStore)
+				Application.OpenURL(urlAmazonStore);
+			else
+				Application.OpenURL(urlAppleStore);
+		});
+	}
+
+}
